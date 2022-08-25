edebd4f9e4296b17f3f53fbaf17ec0b1b8975127
tristan99 <ariescacy9@gmail.com>
2022-08-25 11:47:23 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Card from './Card'
 
 function App() {
+  const [courses, updateCourses] = useState ([]);
+  const API_url = "https://larnu-dev-upy5mhs63a-rj.a.run.app/api/v1/categories";
+
+  function getApiCourses() {
+    return new Promise((resolve, reject) => {
+      fetch(API_url)
+      .then((response) => response.json())
+      .then((data) => {
+        resolve(data.communityCategories)
+      })
+    })
+  }
+
+  async function getCourses() {
+    const courses = await getApiCourses();
+    console.log(courses);
+    updateCourses(courses);
+  }
+
+  useEffect(() => {
+    getCourses();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App_container">
+        {
+          courses.map(e => {
+            return <Card 
+            name = {e.name}
+            icon = {e.icon}
+            background = {e.background}
+            totalQuizzes = {e.totalQuizzes}
+            users = {e.users}
+            key = {e.id}
+            />
+          })
+        }
+      </div>
     </div>
   );
 }
